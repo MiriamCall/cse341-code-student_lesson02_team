f23f42566d5f8678dd70f8561c3168643e98728d
MiriamCall <bow18005@byui.edu>
2025-01-16 18:37:46 -0700

    added code to routes and professional

--- backend/controllers/professional.js
+++ backend/controllers/professional.js
@@ -1,8 +1,18 @@
 const mongodb = require("../database/db_connection");
 
-professionalController.getData = (req, res, next) => {
-  const data = utils.getData(req, res, next);
-  res.json(data);
+const getData = async (req, res, next) => {
+  const results = await mongodb.getDb().db().collection("users").find();
+  result.toArray().then((lists) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(lists[0]); // we just need the first one (the only one)
+  });
 };
 
-module.exports = professionalController;
+module.exports = { getData };
+
+// professionalController.getData = (req, res, next) => {
+//   const data = utils.getData(req, res, next);
+//   res.json(data);
+// };
+
+// module.exports = professionalController;
