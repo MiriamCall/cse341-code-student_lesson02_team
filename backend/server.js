d76fe0a6c088a7554ffb07d3144e4d954b4124a2
MiriamCall <bow18005@byui.edu>
2025-01-16 18:37:20 -0700

    added routes to routes.js and connections in the professional.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectToDatabase = require("./database/db_connection");
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.json()); //used to parse JSON bodies
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectToDatabase()
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database");
+    console.error(error);
+  });
+
+app.use("/api", require("./routes/routes"));
+
+if (process.env.NODE_ENV === "development") {
+  app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+  });
+} else if (process.env.NODE_ENV === "production") {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+} else {
+  console.error("NODE_ENV not set");
+}
